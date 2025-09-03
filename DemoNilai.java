d38b0c726e0f3a5426c79e3f1c2317952183c044
Raihan <raihanfebriahdi18@gmail.com>
2025-09-03 10:25:54 +0700

    Add files via upload

---
+++ DemoNilai.java
@@ -0,0 +1,14 @@
+public class DemoNilai {
+
+    public static void main(String[] args) {
+        
+        // Membuat objek dari class Nilai
+        Nilai nilaiMahasiswa = new Nilai();
+        
+        // Memanggil method untuk memasukkan data
+        nilaiMahasiswa.Nilai();
+        
+        // Memanggil method untuk mencetak hasil
+        nilaiMahasiswa.CetakNilai();
+    }
+}
---
+++ Mahasiswa.java
@@ -0,0 +1,14 @@
+public class Mahasiswa {
+    
+    public void membaca() {
+        System.out.println("Mahasiswa sedang membaca buku.");
+    }
+    
+    public void nyontek() {
+        System.out.println("Mahasiswa sedang menyontek.");
+    }
+    
+    public void modifikasi() {
+        System.out.println("Mahasiswa sedang memodifikasi kode.");
+    }
+}
